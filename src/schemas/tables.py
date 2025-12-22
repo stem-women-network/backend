b0b765f586d3a66f6e51b714d47390312241e645
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2025-12-21 21:33:26 -0300

    feat: criar modelos das tabelas no SQLModel

---
+++ src/schemas/tables.py
@@ -0,0 +1,153 @@
+from datetime import datetime
+from pydantic import EmailStr
+from sqlalchemy.orm import RelationshipDirection
+from sqlalchemy.orm.relationships import _RelationshipDeclared
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, table
+
+
+class Usuario(SQLModel, table=True):
+    id_usuario : int = Field(primary_key=True)
+    nome_completo : str = Field(max_length=100)
+    cpf : str = Field(min_length=11,max_length=11)
+    email : EmailStr = Field()
+    senha : str = Field(min_length=60, max_length=60)
+
+    administradores : list["Administrador"] = Relationship(back_populates="usuario")
+    coordenadores : list["Coordenador"] = Relationship(back_populates="usuario")
+    mentoradas : list["Mentorada"] = Relationship(back_populates="usuario")
+    mentoras : list["Mentora"] = Relationship(back_populates="usuario")
+
+    
+class UniversidadeInstituicao(SQLModel, table=True):
+    id_universidade_instituicao : int = Field(primary_key=True)
+    nome_instituicao : str = Field(max_length=100)
+
+    coordenadores : list["Coordenador"] = Relationship(back_populates="universidade_instituicao")
+    mentoradas : list["Mentorada"] = Relationship(back_populates="universidade_instituicao")
+    mentoras : list["Mentora"] = Relationship(back_populates="universidade_instituicao")
+
+    
+class Administrador(SQLModel, table=True):
+    id_administrador : int = Field(primary_key=True)
+    
+    id_usuario : int = Field(foreign_key="Usuario.id_usuario")
+    
+    usuario : Usuario = Relationship(back_populates="administradores")
+    
+    
+class Coordenador(SQLModel, table=True):
+    id_coordenador : int = Field(primary_key=True)
+
+    id_usuario : int = Field(foreign_key="Usuario.id_usuario")
+    id_universidade_instituicao : int = Field(foreign_key="UniversidadeInstituicao.id_universidade_instituicao")
+    
+    usuario : Usuario = Relationship(back_populates="coordenadores")
+    universidade_instituicao : UniversidadeInstituicao = Relationship(back_populates="coordenadores")
+
+    
+class Mentorada(SQLModel, table=True):
+    id_mentorada : int = Field(primary_key=True)
+    linkedin : str | None = Field(max_length=100)
+    curso_area_stem : str = Field(max_length=100)
+    ano_curso : int = Field(max_digits=4)
+    semestre : int = Field(max_digits=2)
+    objetivo_mentoria : str = Field(max_length=200)
+    disponibilidade : int = Field(max_digits=3)
+    conta_ativa : bool = Field(default = True)
+
+    id_usuario : int = Field(foreign_key="Usuario.id_usuario")
+    id_universidade_instituicao : int = Field(foreign_key="UniversidadeInstituicao.id_universidade_instituicao")
+    
+    usuario : Usuario = Relationship(back_populates="mentoradas")
+    universidade_instituicao : UniversidadeInstituicao = Relationship(back_populates="mentoradas")
+    
+    pedidos : list["PedidosMentoria"] = Relationship(back_populates="mentorada")
+    mentorias : list["Mentoria"] = Relationship(back_populates="mentorada")
+    proximos_encontros : list["Mentoria"] = Relationship(back_populates="mentorada")
+    certificados : list["Certificado"] = Relationship(back_populates="mentorada")
+    encontros : list["Encontro"] = Relationship(back_populates="mentorada")
+    
+
+class Mentora(SQLModel, table=True):
+    id_mentora : int = Field(primary_key=True)
+    linkedin : str | None = Field(max_length=100)
+    formacao : str = Field(max_length=100)
+    cargo_atual : str = Field(max_length=100)
+    area_atuacao : int = Field(max_digits=7)
+    disponibilidade : int = Field(max_digits=3)
+    conta_ativa : bool = Field(default=True)
+
+    id_usuario : int = Field(foreign_key="Usuario.id_usuario")
+    id_universidade_instituicao : int = Field(foreign_key="UniversidadeInstituicao.id_universidade_instituicao")
+    usuario : Usuario = Relationship(back_populates="mentoras")
+    universidade_instituicao : UniversidadeInstituicao = Relationship(back_populates="mentoras")
+
+    pedidos : list["PedidosMentoria"] = Relationship(back_populates="mentora")
+    mentorias : list["Mentoria"] = Relationship(back_populates="mentora")
+    proximos_encontros : list["ProximoEncontro"] = Relationship(back_populates="mentora")
+    encontros : list["Encontro"] = Relationship(back_populates="mentora")
+    
+
+class PedidosMentoria(SQLModel, table=True):
+    id_pedidos_mentoria : int = Field(primary_key=True)
+    estado_pedido : str = Field(max_length=10)
+    ano_pedido : int = Field(max_digits=4)
+    
+    id_mentora : int = Field(foreign_key="Mentora.id_mentora")
+    id_mentorada : int = Field(foreign_key="Mentorada.id_mentorada")
+    mentora : Mentora = Relationship(back_populates="pedidos")
+    mentorada : Mentorada = Relationship(back_populates="pedidos")
+
+    
+class Mentoria(SQLModel, table=True):
+    id_mentoria : int = Field(primary_key=True)
+    estado_mentoria : str = Field(max_length=20)
+    avaliacao_mentora : str | None = Field(max_length=500)
+    avaliacao_mentorada : str | None = Field(max_length=500)
+    nota_mentora : int | None = Field(max_digits=2)
+    nota_mentorada : int | None = Field(max_digits=2)
+    
+    id_mentora : int = Field(foreign_key="Mentora.id_mentora")
+    id_mentorada : int = Field(foreign_key="Mentorada.id_mentorada")
+    
+    mentora : Mentora = Relationship(back_populates="mentorias")
+    mentorada : Mentorada = Relationship(back_populates="mentorias")
+
+    
+class ProximoEncontro(SQLModel, table=True):
+    id_proximo_encontro : int = Field(primary_key=True)
+    data_sugerida : datetime = Field()
+    topico_sugerido : str = Field(max_length=100)
+    
+    id_mentora : int = Field(foreign_key="Mentora.id_mentora")
+    id_mentorada : int = Field(foreign_key="Mentorada.id_mentorada")
+    
+    mentora : Mentora = Relationship(back_populates="proximos_encontros")
+    mentorada : Mentorada = Relationship(back_populates="proximos_encontros")
+
+    
+class Encontro(SQLModel, table=True):
+    id_encontro : int = Field(primary_key=True)
+    data_encontro : datetime = Field()
+    duracao_min : int = Field()
+    tema : str = Field(max_length=100)
+    topicos_discutidos : str = Field(max_length=300)
+    progresso_mentorada : int = Field(gt=1, le=5)
+    observacoes : str = Field(max_length=200)
+
+    id_mentora : int = Field(foreign_key="Mentora.id_mentora")
+    id_mentorada : int = Field(foreign_key="Mentorada.id_mentorada")
+    
+    mentora : Mentora = Relationship(back_populates="encontros")
+    mentorada : Mentorada = Relationship(back_populates="encontros")
+
+    
+class Certificado(SQLModel, table=True):
+    id_certificado : int = Field(primary_key=True)
+    ano_certificado : int = Field(max_digits=4)
+    
+    id_mentorada : int = Field(foreign_key="Mentorada.id_mentorada")
+    
+    mentorada : Mentorada = Relationship(back_populates="certificados")
+    
+    
