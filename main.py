71f4529cf3e54365b73852f2b9140b09ccd53a98
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2026-01-23 19:34:29 -0300

    feat: adicionar CORSMiddleware a API

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from src.routers import mentors, users, match, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="STEM Women Backend")
+app.add_middleware(CORSMiddleware,
+                   allow_origins="*",
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   )
 
 app.include_router(mentors.router, prefix="/mentors", tags=["Mentores"])
 app.include_router(match.router, prefix="/match", tags=["Match"])
