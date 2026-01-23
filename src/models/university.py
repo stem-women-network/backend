18a02fa0e264eb91b51eb63f0c4ec233c5718209
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2026-01-23 18:56:01 -0300

    fix: criar modelo universidade que estava faltando

---
+++ src/models/university.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from sqlmodel import Date, Session, select
+from src.database import engine
+from src.schemas.tables import UniversidadeInstituicao
+
+
+class UniversityModel(BaseModel):
+    nome_instituicao : str
+
+
+class University:
+    @classmethod
+    def create_university(cls, universityModel : UniversityModel):
+        session = Session(engine)
+        try:
+            university = UniversidadeInstituicao(
+                nome_instituicao=universityModel.nome_instituicao
+            )
+            session.add(university)
+            session.commit()
+            session.close()
+        except Exception as e:
+            print(e)
+            return (None, None)
+        finally:
+            session.close()
+        return (None, None)
